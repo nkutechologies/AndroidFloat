543f1d2f4257e7c4b7efd4d9b746e5ac819f8df9
nkutechologies <71810407+nkutechologies@users.noreply.github.com>
2022-05-26 02:56:55 -0700

    ahmad

--- Src/Components/DropDownComponent.js
+++ Src/Components/DropDownComponent.js
@@ -1,51 +1,48 @@
-import React, { Component,useState } from 'react';
-import { View, Text, TextInput, Image } from 'react-native';
+import React, { Component, useState, useRef } from 'react';
+import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native';
 import Theme from '../Utils/Theme';
 import { Icon } from 'react-native-elements'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { useLinkProps } from '@react-navigation/native';
 import DropDownPicker from 'react-native-dropdown-picker';
+import ModalDropdown from 'react-native-modal-dropdown';
 const DropDownComponent = (props) => {
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(['italy', 'spain', 'barcelona', 'finland']);
-    const [items, setItems] = useState([
-      {label: 'Spain', value: 'spain'},
-      {label: 'Madrid', value: 'madrid', parent: 'spain'},
-      {label: 'Barcelona', value: 'barcelona', parent: 'spain'},
-  
-      {label: 'Italy', value: 'italy'},
-      {label: 'Rome', value: 'rome', parent: 'italy'},
-  
-      {label: 'Finland', value: 'finland'}
-    ]);
-  return (
-    <View style={{borderBottomWidth:0.3}}>
-      <Text style={{color:Theme.black,marginTop:Theme.screenHeight/90,
-    fontSize:Theme.screenHeight/50,fontWeight:'bold'}}>{props.Title}</Text>
-    <View
-      style={{
-        backgroundColor: '#fff',
-        flexDirection: 'row',
-        // elevation: 5,
-        // justifyContent: 'space-between',
-        alignItems: 'center',
-        borderRadius: 10,
-        // paddingLeft: 4,
-      }}>
-     <DropDownPicker
-        open={props.Iconopen}
-        value={props.value}
-        items={props.items}
-        setOpen={props.setOpen}
-        setValue={props.setValue}
-        setItems={props.setItems}
+    const dropdown = useRef(null);
+    return (
+        <View style={{ borderBottomWidth: 0.3, paddingBottom: Theme.screenHeight / 80 }}>
+            <Text style={{
+                color: Theme.black, marginVertical: Theme.screenHeight / 90,
+                fontSize: Theme.screenHeight / 50, fontWeight: 'bold'
+            }}>{props.Title}</Text>
+            <View
+                style={{
+                    backgroundColor: '#fff',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    // borderRadius: 10,
+                    justifyContent: 'space-between',
+                    paddingHorizontal: Theme.screenWidth / 90
+                }}>
+                <ModalDropdown
+                    ref={dropdown}
+                    options={props.options}
+                    defaultValue={props.defaultValue}
+                    dropdownStyle={props.dropdownStyle}
+                    style={props.dropDownContainerStyle}
+                    textStyle={{ color: Theme.black }}
+                    dropdowntextStyle={{}}
+                    onSelect={(index, value) => props.onSelect(index, value)}
+                />
+                <TouchableOpacity onPress={() => dropdown.current.show()}>
+                    <Icon
+                        name={'chevron-down'}
+                        type={'feather'}
+                    />
+                </TouchableOpacity>
 
-       
-      />
-   
-     
-    </View>
-    </View>
-  );
+
+            </View>
+        </View>
+    );
 };
 export default DropDownComponent;
--- Src/Screens/MainFlow/ConsumerInter.js
+++ Src/Screens/MainFlow/ConsumerInter.js
@@ -1,41 +1,39 @@
 //import liraries
-import React, { Component,useState } from 'react';
-import { View, Text, StyleSheet,ScrollView } from 'react-native';
+import React, { Component, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Theme from '../../Utils/Theme';
 import TextComponent from '../../Components/TextComponent';
+import DropDownComponent from '../../Components/DropDownComponent';
 import ButtonComponent from '../../Components/ButtonComponent';
 import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import DropDownPicker from 'react-native-dropdown-picker';
+import ModalDropdown from 'react-native-modal-dropdown';
+import { useLinkProps } from '@react-navigation/native';
 // create a component
 const ConsumerInter = () => {
-    const [value, setValue] = useState(['italy', 'spain', 'barcelona', 'finland']);
-    const [open, setOpen] = useState(false);
-     const [items, setItems] = useState([
-    {label: 'Spain', value: 'spain'},
-    {label: 'Madrid', value: 'madrid', parent: 'spain'},
-    {label: 'Barcelona', value: 'barcelona', parent: 'spain'},
-
-    {label: 'Italy', value: 'italy'},
-    {label: 'Rome', value: 'rome', parent: 'italy'},
-
-    {label: 'Finland', value: 'finland'}
-  ]);
-    const [username, setUserName] = useState();
-    const [password, setPassword] = useState();
+    const [Vendor, setVendor] = useState();
+    const [name, setName] = useState();
+    const [CNIC, setCNIC] = useState();
+    const [age, setage] = useState();
+    const [cellNo, setcellNo] = useState();
+    const [address, setaddress] = useState();
     const [see, setSee] = useState(true)
     return (
         <View style={styles.container}>
-              <ScrollView showsVerticalScrollIndicator={false}>
-              <Card elevation={5} style={{alignItems:'center',backgroundColor:'#f0f0f0'
-            }}>
-               
-               <View style={{height:Theme.screenHeight/11,justifyContent:'center'}}>
-               <Text style={{fontSize:Theme.screenHeight/35,
-                color:Theme.black,}}>Consumer Data Form</Text>
-               </View>
-                
-               </Card>
-               <View style={{
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Card elevation={5} style={{
+                    alignItems: 'center', backgroundColor: '#f0f0f0'
+                }}>
+
+                    <View style={{ height: Theme.screenHeight / 11, justifyContent: 'center' }}>
+                        <Text style={{
+                            fontSize: Theme.screenHeight / 35,
+                            color: Theme.black,
+                        }}>Consumer Data Form</Text>
+                    </View>
+
+                </Card>
+                {/* <View style={{
       backgroundColor: '#171717',
       flex: 1,
       alignItems: 'center',
@@ -50,142 +48,127 @@ const ConsumerInter = () => {
         setValue={setValue}
         setItems={setItems}
 
-        theme="DARK"
-        multiple={true}
+        // theme="DARK"
+        // multiple={true}
         mode="BADGE"
         badgeDotColors={["#e76f51", "#00b4d8", "#e9c46a", "#e76f51", "#8ac926", "#00b4d8", "#e9c46a"]}
       />
-    </View>
-            <View style={{paddingHorizontal:Theme.screenWidth/20,marginVertical:Theme.screenHeight/30}}>
-            <View style={styles.inputView}>
-                    <TextComponent
+    </View> */}
+
+                <View style={{ paddingHorizontal: Theme.screenWidth / 20, marginVertical: Theme.screenHeight / 30 }}>
+                    <View style={styles.inputView}>
+                        <DropDownComponent
+                            Title={'Territor'}
+                            options={['Ahmad', 'Goraya']}
+                            defaultValue={'please Select'}
+                            dropdownStyle={styles.dropdownStyle}
+                            IconName={'angle-down'}
+                            IconType={'font-awesome-5'}
+                            onSelect={(index, value) => { setVendor({}) }}
+                        />
+                        {/* <DropDownComponent
                         Title={'Territor'}
-                        source="mail"
-                        placeholder="Enter your email"
-                        value={username}
-                        // onChangeText={(username) => setUserName(username)}
-                        name={'down'}
-                        type={"antdesign"}
-                        iconPress={() => setSee(!see)}
-                    />
-                </View>
-                <View style={styles.passwordView}>
-                    <TextComponent
-                        Title={'Down'}
-                        iconPress={() => setSee(!see)}
-                        // source={see ? "ios-eye-off-sharp" : "eye"}
-                        placeholder="Enter your password"
-                        value={password}
-                        secureTextEntry={see ? true : false}
-                        onChangeText={(password) => setPassword(password)}
-                        name={'down'}
-                        type={"antdesign"}
-                    />
-                </View>
-                <View style={styles.passwordView}>
-                    <TextComponent
-                        Title={'Name'}
-                        iconPress={() => setSee(!see)}
-                        // source={see ? "ios-eye-off-sharp" : "eye"}
-                        placeholder="Enter your name"
-                        value={password}
-                        secureTextEntry={see ? true : false}
-                        onChangeText={(password) => setPassword(password)}
-                      
-                    />
-                </View>
-                <View style={styles.passwordView}>
-                    <TextComponent
-                        Title={'CNIC'}
-                        iconPress={() => setSee(!see)}
-                        // source={see ? "ios-eye-off-sharp" : "eye"}
-                        placeholder="Enter your cnic"
-                        value={password}
-                        secureTextEntry={see ? true : false}
-                        onChangeText={(password) => setPassword(password)}
-                      
-                    />
-                </View>
-                <View style={styles.passwordView}>
-                    <TextComponent
-                        Title={'Cell No'}
-                        iconPress={() => setSee(!see)}
-                        // source={see ? "ios-eye-off-sharp" : "eye"}
-                        placeholder="Enter your number"
-                        value={password}
-                        secureTextEntry={see ? true : false}
-                        onChangeText={(password) => setPassword(password)}
-                      
-                    />
-                </View>
-                <View style={styles.passwordView}>
-                    <TextComponent
-                        Title={'Age'}
-                        iconPress={() => setSee(!see)}
-                        // source={see ? "ios-eye-off-sharp" : "eye"}
-                        placeholder="Enter your age"
-                        value={password}
-                        secureTextEntry={see ? true : false}
-                        onChangeText={(password) => setPassword(password)}
-                      
-                    />
-                </View>
-                <View style={styles.passwordView}>
-                    <TextComponent
-                        Title={'Address'}
-                        iconPress={() => setSee(!see)}
-                        // source={see ? "ios-eye-off-sharp" : "eye"}
-                        placeholder="Enter your address"
-                        value={password}
-                        secureTextEntry={see ? true : false}
-                        onChangeText={(password) => setPassword(password)}
-                      
-                    />
-                </View>
-                <View style={styles.passwordView}>
-                    <TextComponent
-                        Title={'Current Brand'}
-                        iconPress={() => setSee(!see)}
-                        // source={see ? "ios-eye-off-sharp" : "eye"}
-                        placeholder="Enter your brand"
-                        value={password}
-                        secureTextEntry={see ? true : false}
-                        onChangeText={(password) => setPassword(password)}
-                        name={'down'}
-                        type={"antdesign"}
-                    />
-                </View>
-                <View style={styles.passwordView}>
-                    <TextComponent
-                        Title={'Target Brand'}
-                        iconPress={() => setSee(!see)}
-                        // source={see ? "ios-eye-off-sharp" : "eye"}
-                        placeholder="Enter your brand"
-                        value={password}
-                        secureTextEntry={see ? true : false}
-                        onChangeText={(password) => setPassword(password)}
-                        name={'down'}
-                        type={"antdesign"}
-                    />
-                </View>
-                <View style={styles.passwordView}>
-                    <TextComponent
-                        Title={'Product Status'}
-                        iconPress={() => setSee(!see)}
-                        // source={see ? "ios-eye-off-sharp" : "eye"}
-                        placeholder="Enter your brand"
-                        value={password}
-                        secureTextEntry={see ? true : false}
-                        onChangeText={(password) => setPassword(password)}
-                        name={'down'}
-                        type={"antdesign"}
-                    />
-                </View>
-                <ButtonComponent
-                    text="Submit"
+                        open={open}
+                        value={value}
+                        items={items}
+                        setOpen={setOpen}
+                        setValue={setValue}
+                        setItems={setItems}
+                    /> */}
+                    </View>
+                    <View style={styles.passwordView}>
+                        <DropDownComponent
+                            Title={'Down'}
+                            options={['Ahmad', 'Goraya']}
+                            defaultValue={'please Select'}
+                            IconName={'angle-down'}
+                            IconType={'font-awesome-5'}
+                            dropdownStyle={styles.dropdownStyle}
+                            onSelect={(index, value) => { setVendor({}) }}
+                        />
+                    </View>
+                    <View style={styles.passwordView}>
+                        <TextComponent
+                            Title={'Name'}
+                            placeholder="Enter your name"
+                            value={name}
+                            onChangeText={(password) => setName(password)}
+
+                        />
+                    </View>
+                    <View style={styles.passwordView}>
+                        <TextComponent
+                            Title={'CNIC'}
+                            placeholder="Enter your cnic"
+                            value={CNIC}
+                            onChangeText={(password) => setCNIC(password)}
+                        />
+                    </View>
+                    <View style={styles.passwordView}>
+                        <TextComponent
+                            Title={'Cell No'}
+                            placeholder="Enter your number"
+                            value={cellNo}
+                            onChangeText={(password) => setcellNo(password)}
+
+                        />
+                    </View>
+                    <View style={styles.passwordView}>
+                        <TextComponent
+                            Title={'Age'}
+                            placeholder="Enter your age"
+                            value={age}
+                            onChangeText={(password) => setage(password)}
+
+                        />
+                    </View>
+                    <View style={styles.passwordView}>
+                        <TextComponent
+                            Title={'Address'}
+                            placeholder="Enter your address"
+                            value={address}
+                            onChangeText={(password) => setaddress(password)}
+
+                        />
+                    </View>
+                    <View style={styles.passwordView}>
+                    <DropDownComponent
+                            Title={'Current Brand'}
+                            options={['Ahmad', 'Goraya']}
+                            defaultValue={'please Select'}
+                            IconName={'angle-down'}
+                            IconType={'font-awesome-5'}
+                            dropdownStyle={styles.dropdownStyle}
+                            onSelect={(index, value) => { setVendor({}) }}
+                        />
+                    </View>
+                    <View style={styles.passwordView}>
+                    <DropDownComponent
+                            Title={'Target Brand'}
+                            options={['Ahmad', 'Goraya']}
+                            defaultValue={'please Select'}
+                            IconName={'angle-down'}
+                            IconType={'font-awesome-5'}
+                            dropdownStyle={styles.dropdownStyle}
+                            onSelect={(index, value) => { setVendor({}) }}
+                        />
+                    </View>
+                    <View style={styles.passwordView}>
+                    <DropDownComponent
+                            Title={'Productive Status'}
+                            options={['Ahmad', 'Goraya']}
+                            defaultValue={'please Select'}
+                            IconName={'chevron-down'}
+                            IconType={'feather'}
+                            dropdownStyle={styles.dropdownStyle}
+                            onSelect={(index, value) => { setVendor({}) }}
+                        />
+                    </View>
+                    <ButtonComponent
+                        text="Submit"
                     // onPress={() => props.navigation.navigate('Home')}
-                />
-            </View>
+                    />
+                </View>
             </ScrollView>
         </View>
     );
@@ -199,8 +182,8 @@ const styles = StyleSheet.create({
         // alignItems: 'center',
         backgroundColor: Theme.white,
     },
-    consumerTextStyle:{
-        
+    consumerTextStyle: {
+
     },
     inputView: {
         // marginTop: Theme.screenHeight / 40,
@@ -211,6 +194,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         // padding: 15,
     },
+    dropdownStyle:{
+        width:Theme.screenWidth/1.5
+    }
 });
 
 //make this component available to the app
